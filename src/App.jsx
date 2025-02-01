7f7459d5044681e264b5f2167782d52da46a013d
Lena Messerschmidt <messerschmidt.lena@gmail.com>
2025-02-01 21:52:55 +0100

    5 minute party level change

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
+
+const partyLevel = [
+  "standing still",
+  "standing vibing",
+  "standing vibing and looking at you",
+  "dancing at the bar",
+  "one small group on the dance floor",
+  "one group and a few singles on the dance floor",
+  "few groups on the dance floor",
+  "dance floor is medium filled",
+  "dance floor is well filled",
+  "dance floor is cramped",
+  "everybody even at the bar is dancing",
+  "they dance everywhere, around you and on the bar",
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentLevel, setCurrentLevel] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      let number = Math.random();
+      if (number < 0.5 && currentLevel < 11) {
+        setCurrentLevel(currentLevel + 1);
+      } else if (number >= 0.5 && currentLevel > 0) {
+        setCurrentLevel(currentLevel - 1);
+      } else {
+        if (currentLevel == 11) {
+          setCurrentLevel(10);
+        } else {
+          setCurrentLevel(1);
+        }
+      }
+    }, 300000);
+
+    //Clearing the interval
+    return () => clearInterval(interval);
+  }, [currentLevel]);
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>{partyLevel[currentLevel]}</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
